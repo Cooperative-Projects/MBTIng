e6de5022793a821598bdbea927644332bf318f23
hhtboy <59467684+hhtboy@users.noreply.github.com>
2023-03-19 21:15:20 +0900

    initial commit

---
+++ mbting/src/main/java/kau/coop/mbting/MbtingApplication.java
@@ -0,0 +1,13 @@
+package kau.coop.mbting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtingApplication.class, args);
+	}
+
+}
---
+++ mbting/src/test/java/kau/coop/mbting/MbtingApplicationTests.java
@@ -0,0 +1,13 @@
+package kau.coop.mbting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
