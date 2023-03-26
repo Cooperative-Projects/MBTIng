41a18dc0b12fa8c5035f36ec34fe6a65da0d86f2
hhtboy <hhtboy1024@gmail.com>
2023-03-26 16:17:21 +0900

    web전용으로 테스트 코드 수정

--- mbting/src/test/java/kau/coop/mbting/scan/AutoAppConfigTest.java
+++ mbting/src/test/java/kau/coop/mbting/scan/AutoAppConfigTest.java
@@ -6,6 +6,7 @@ import kau.coop.mbting.service.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
@@ -14,7 +15,7 @@ public class AutoAppConfigTest {
 
     @Test
     void basicScan() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        AnnotationConfigServletWebApplicationContext ac = new AnnotationConfigServletWebApplicationContext(AutoAppConfig.class);
 
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
